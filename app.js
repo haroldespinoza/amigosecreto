014627aa244161d20d7fc37d85179b0291181bd0
Harold Espinoza <hespinoza@tns.cl>
2025-02-04 13:16:55 -0300

    creacion de script

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []; //declara variable amigos
 let amigosSorteados = new Set();
+
+function agregarAmigo () {
+    let input = document.getElementById("amigo");
+    let amigo = input.value.trim();
+
+    function esTextoValido(texto) {
+        return /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/.test(texto);
+    }
+
+    if (amigo === "") {
+        alert("Por favor, ingresa un nombre válido");
+        return;
+    }
+
+    if (!esTextoValido(amigo)) {
+        alert("solo se permite letras y espacios. No ingreses números ni caracteres especiales");
+        return;
+
+    }
+    amigos.push(amigo);
+    input.value = "";
+    mostrarLista();
+}
+
+function mostrarLista(){
+    let lista = DocumentTimeline.getElementById("listaAmigos");
+    lista.innerHtml ="";
+    amigos.forEach(amigo => {
+        let li =document.createElement("li");
+        li.textContent = amigo;
+        li.className = "amigo-item";
+        lista.appendChild(li);
+    });
+}
