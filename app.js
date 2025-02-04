6c7beabff78e36fb4a9c42d5a0824b0f09dc5c73
Harold Espinoza <hespinoza@tns.cl>
2025-02-04 13:40:41 -0300

    Correccion error mostrar lista de amigos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []; //declara variable amigos
+let amigos = []; 
 let amigosSorteados = new Set();
 
 function agregarAmigo() {
